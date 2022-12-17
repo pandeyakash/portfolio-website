56b4e68b21ea16fba6d9f736b40ca689ae283c48
pandeyakash <asmyselfakash80@gmail.com>
2022-12-17 16:46:27 +0530

    Created All Components

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+
+// ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.createRoot(<App/>, document.querySelector('#root'))
+
+// const rootElement = document.queryselector('#root');
+// const root = createRoot(rootElement);
